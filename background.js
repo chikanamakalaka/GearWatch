d2ae0d56ba5d08288f71e58164a2ad82938277c9
Matt Hocker <mhocker@gmail.com>
2014-07-22 05:55:56 -0700

    Shrank Clock

--- background.js
+++ background.js
@@ -10,8 +10,8 @@ chrome.app.runtime.onLaunched.addListener(function() {
     {
       id: "mainWindow",
       bounds: {
-        width: 500,
-        height: 300
+        width: 1036,
+        height: 653
       }
     }
   );
